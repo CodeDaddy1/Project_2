f2c962dfd775adbab36568287ab97334ad66495d
CodeDaddy <146156197+CodeDaddy1@users.noreply.github.com>
2023-11-28 21:13:52 +0000

    Program Complete

--- project_2.cpp
+++ project_2.cpp
@@ -1,1 +1,237 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+float const PI = 3.14;
+
+bool isInteger(const std::string& input) {
+	std::istringstream iss(input);
+	int value; 
+	return iss >> value && iss.eof();
+}
+
+bool isDouble(const std::string& input) {
+	std::istringstream iss(input);
+	double value; 
+	return iss >> value && iss.eof();
+}
+
+
+void promptMenu(){
+
+std::cout << "Please select from the following: " << std::endl;
+std::cout << "1: Calculate the area of a circle " << std::endl;
+std::cout << "2: Calculate the tax based on item price and tax rate " << std::endl;
+std::cout << "3: Convert pound to kilogram " << std::endl;
+std::cout << "4: Quit program " << std::endl;
+std::cout << std::endl;
+}
+
+void calculateCircleArea(){
+    std::string name;
+    double radius;
+
+    std::cout << std::fixed << std::setprecision(2) << std::endl;
+    std::cout << "What is your name? " << std::endl;
+    std::cin.ignore();
+    std::getline(std::cin, name);
+
+    std::cout << std::endl;
+
+    std::cout << "What is the radius of the circle? " << std::endl; 
+    std::cin >> radius;
+
+    std::cout << std::endl;
+    double area = (radius * radius) * PI;
+
+    std::cout << "Your name is " << name << "." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Aread of the circle is " << area << "." << std::endl;
+    std::cout << std::endl;
+
+}
+
+void calculateTax(){
+    std::string name;
+    std::string itemPriceStr;
+    std::string taxRateStr;
+    double itemPrice;
+    double taxRate;
+
+    std::cout << std::fixed << std::setprecision(2) << std::endl;
+    std::cout << "What is your name? " << std::endl;
+    std::cin.ignore();
+    std::getline(std::cin, name);
+
+    do{
+
+    std::cout << "What is the price of the item? " << std::endl;
+    std::getline(std::cin , itemPriceStr);
+
+        if(isDouble(itemPriceStr)){
+            itemPrice = std::stod(itemPriceStr);
+            if(itemPrice > 0){
+                break;
+            }
+        }
+        else {
+                std::cout << "Invalid choice please enter a dollar amount value. " << std::endl;
+            }
+
+    }while(true);
+    std::cout << std::endl;
+
+    do{
+    std::cout << "What is the tax rate? " << std::endl;
+    std::getline(std::cin , taxRateStr);
+
+        if(isDouble(taxRateStr)){
+            taxRate = std::stod(taxRateStr);
+            if(taxRate > 0){
+                break;
+            }
+        }
+        else {
+                std::cout << "Invalid choice please enter a tax amount value. " << std::endl;
+            }
+    }while(true);
+    std::cout << std::endl;
+    double taxAmount = (itemPrice * taxRate) / 100;
+
+    std::cout << "Your name is " << name << "." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Your tax amount is $" << taxAmount << std::endl;
+    std::cout << std::endl;
+}
+
+void poundToKilogram(){
+    std::string name;
+    std::string userPoundsStr;
+    double userPounds;
+
+    std::cout << std::fixed << std::setprecision(2) << std::endl;
+    std::cout << "What is your name ?" << std::endl;
+    std::cin.ignore();
+    std::getline(std::cin, name);
+    do{
+    std::cout << "What is your weight in pounds? " << std::endl;
+    std::getline(std::cin, userPoundsStr);
+        if(isDouble(userPoundsStr)){
+            userPounds = std::stod(userPoundsStr);
+            if(userPounds >= 0){
+                break;
+            }
+        }
+        else {
+            std::cout << "Invalid input, please enter a numerical value representing weight in pounds. " << std::endl;
+        }
+    }while(true);    
+    std::cout << std::endl;
+    double userKilograms = userPounds / 2.2046;
+
+    std::cout << "Your name is " << name << "." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Your weight in kilograms is " << userKilograms << "kg " << std::endl;
+    std::cout << std::endl;
+
+}
+
+int terminateProgram(){
+    return 0;
+}
+
+
+int main(){
+
+std::string repeatProgram;
+int programChoice;
+std::string programChoiceStr;
+
+do{
+promptMenu();
+
+std::getline(std::cin, programChoiceStr);
+        
+		if (isInteger(programChoiceStr)) {
+			programChoice = std::stoi(programChoiceStr);
+			if (programChoice >= 1 && programChoice <= 5) {
+				break;
+            }
+        }
+else {
+    std::cout << "You have chosen an invalid selection, please try again. " << std::endl;
+    
+}
+
+}while(true);
+    switch(programChoice){
+    
+        case 1:
+       calculateCircleArea();
+        break;
+
+        case 2:
+        calculateTax();
+        break;
+
+        case 3:
+        poundToKilogram();
+        break;
+
+        case 4:
+        terminateProgram();
+
+        break;
+
+        default: std::cout << "Invalid choice, please try again." << std::endl;
+    }   
+
+
+do{
+std::cout << "Would you like to repeat the program? " << std::endl;
+std::cout << "y or Y to continue. " << std::endl;
+std::cout << std::endl;
+
+std::cin >> repeatProgram;
+
+    if(repeatProgram == "y" || repeatProgram == "Y"){
+        promptMenu();
+
+        std::cin >> programChoice;
+
+    switch(programChoice){
+    
+        case 1:
+       calculateCircleArea();
+        break;
+
+        case 2:
+        calculateTax();
+        break;
+
+        case 3:
+        poundToKilogram();
+        break;
+
+        case 4:
+        terminateProgram();
+
+        break;
+
+        default: std::cout << "Invalid choice, please try again." << std::endl;
+    }   
+
+    }
+    else if(repeatProgram == "n" || repeatProgram == "N"){
+        std::cout << "You have chosen to end the program. Goodbye! " << std::endl;
+        return 0;
+    }
+    else {
+        std::cout << "Invalid choice, please try again. " << std:: endl;
+        std::cout << std::endl;
+    }
+}while(true);
+    return 0;
+}
 
